dbf4531921a04fd1aeb7224870c7715844901974
Jon Jagger <jon@jaggersoft.com>
2020-01-18 20:41:53 +0000

    Upgrade build scripts

---
+++ docker/install.sh
@@ -0,0 +1,7 @@
+#!/bin/bash -Eeu
+
+apk --update add --virtual build-dependencies build-base
+echo "gem: --no-rdoc --no-ri" > ~/.gemrc 
+gem install rspec approvals
+apk del build-dependencies build-base
+rm -vrf /var/cache/apk/*
