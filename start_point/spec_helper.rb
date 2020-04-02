24d9986742ed7cd2577e0625e318caa33411055e
Jon Jagger <jon@jaggersoft.com>
2020-04-02 09:11:21 +0100

    Tidy file layout

--- start_point/spec_helper.rb
+++ start_point/spec_helper.rb
@@ -7,6 +7,10 @@ Approvals.configure do |cfg|
 end
 
 RSpec.configure do |cfg|
-  cfg.add_setting :approvals_namer_class, :default => Approvals::Namers::RSpecNamer
-  cfg.add_setting :approvals_path, :default => './'
+  cfg.add_setting :approvals_namer_class, {
+    :default => Approvals::Namers::RSpecNamer
+  }
+  cfg.add_setting :approvals_path, {
+    :default => './'
+  }
 end
