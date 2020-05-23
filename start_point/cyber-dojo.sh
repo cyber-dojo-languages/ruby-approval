6420057ac79540b7b86c4c1007baa73fcfd7852e
Jon Jagger <jon@jaggersoft.com>
2020-05-23 11:01:17 +0100

    Make dir names explicit in cyber-dojo.sh trap handler

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -4,7 +4,7 @@ trap tidy_up EXIT
 function tidy_up()
 {
   [ ! -f .approvals ] || rm .approvals
-  [ ! -d coverage ] || rm -rf coverage
+  [ ! -d coverage/ ] || rm -rf coverage/
 }
 
 # remove existing coverage report if there is one
