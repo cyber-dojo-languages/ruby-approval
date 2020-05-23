6709ce68b27b4c405a0cb2e7439152dadd9223d6
Jon Jagger <jon@jaggersoft.com>
2020-05-23 14:43:57 +0100

    Use more explicit tidy_up() in cyber-dojo.sh

--- start_point/cyber-dojo.sh
+++ start_point/cyber-dojo.sh
@@ -1,15 +1,35 @@
 set -e
 
+# ------------------------------------------------------------------------
+# cyber-dojo returns text files under /sandbox that are
+# created/deleted/changed. In tidy_up you can remove any
+# such files you don't want returned to the browser.
+
 trap tidy_up EXIT
+
 function tidy_up()
 {
-  # cyber-dojo returns text files under /sandbox that are
-  # created/deleted/changed. In here you can remove any
-  # such files you don't want returned to the browser.
-  [ ! -f .approvals ] || rm .approvals
-  [ ! -d coverage/ ] || rm -rf coverage/
+  delete_dirs coverage/
+  delete_files .approvals
 }
 
+function delete_dirs()
+{
+  for dirname in "$@"
+  do
+      rm -rf "${dirname}" 2> /dev/null || true
+  done
+}
+
+function delete_files()
+{
+  for filename in "$@"
+  do
+      rm "${filename}" 2> /dev/null || true
+  done
+}
+
+# ------------------------------------------------------------------------
 # remove existing coverage report if there is one
 [ -e report/coverage.txt ] && rm report/coverage.txt
 
