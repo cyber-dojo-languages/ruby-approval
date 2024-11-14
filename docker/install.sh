c7afb3e64e46757a3f2aabd1bcc727f951fcbf8e
Faye <faye@kosli.com>
2024-11-14 16:04:36 +0100

    Try another way of installing nokogiri for arm64 images

--- docker/install.sh
+++ docker/install.sh
@@ -1,6 +1,7 @@
 #!/bin/bash -Eeu
 
 apk --update add --virtual build-dependencies build-base
-gem install rspec approvals nokogiri --no-document
+gem install rspec approvals --no-document
+NOKOGIRI_USE_SYSTEM_LIBRARIES=1 gem install nokogiri
 apk del build-dependencies build-base
 rm -vrf /var/cache/apk/*
