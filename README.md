80ea2b1958437303608881f69d8d2e21a8abadea
JonJagger <jon@kosli.com>
2026-02-07 09:08:51 +0000

    Upgrade to Ruby 4.0.1, approvals 0.1.7, change default branch to main

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 [![Github Action (main)](https://github.com/cyber-dojo-languages/ruby-approval/actions/workflows/main.yml/badge.svg)](https://github.com/cyber-dojo-languages/ruby-approval/actions)
 
-## display_name="Ruby 3.4.1, Approvals 0.0.26"
+## display_name="Ruby 4.0.1, Approvals 0.1.7"
 ## image_name="ghcr.io/cyber-dojo-languages/ruby_approval"
 
 ![cyber-dojo.org home page](https://github.com/cyber-dojo/cyber-dojo/blob/master/shared/home_page_snapshot.png)
---
+++ check_version.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+set -Eeu
+
+readonly MY_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+readonly REGEX="image_name\": \"(.*)\""
+readonly JSON=`cat ${MY_DIR}/docker/image_name.json`
+[[ ${JSON} =~ ${REGEX} ]]
+readonly IMAGE_NAME="${BASH_REMATCH[1]}"
+
+readonly EXPECTED="ruby 0.1.7"
+readonly ACTUAL=$(docker run --rm -i ${IMAGE_NAME} sh -c 'gem list | grep approvals')
+
+if echo "${ACTUAL}" | grep -q "${EXPECTED}"; then
+  echo "VERSION CONFIRMED as ${EXPECTED}"
+else
+  echo "VERSION EXPECTED: ${EXPECTED}"
+  echo "VERSION   ACTUAL: ${ACTUAL}"
+  exit 42
+fi
